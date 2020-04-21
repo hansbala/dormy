8efba58268905acf6d49d92ae230c25b46a38bfb
Hans Bala <hansbala@Hanss-MacBook-Pro.local>
2020-04-21 18:35:40 +0530

    initial commit

---
+++ App.vue
@@ -0,0 +1,54 @@
+<template>
+  <app-navigator></app-navigator>
+</template>
+
+<script>
+import {
+  createAppContainer,
+  createStackNavigator,
+  createSwitchNavigator,
+  createDrawerNavigator
+} from "vue-native-router";
+
+import StartScreen from "./Start.vue";
+import HomeScreen from "./Screens/Home/Home.vue";
+
+// Will need the switch navigator for the login flow 
+// to essentially remove the "Back" button from different
+// login screens. The login/signup navigator will 
+// probably be wrapped inside the drawer and tabbed navigator
+
+// const StackNavigator = createSwitchNavigator(
+//   {
+//     Start: StartScreen,
+//     Home: HomeScreen,
+//   },
+//   {
+//     initialRouteName: 'Start',
+//   }
+// );
+
+const DrawerNavigator = createDrawerNavigator(
+  {
+    Start: StartScreen,
+    Home: HomeScreen
+  },
+  {
+    initialRouteName: 'Home'
+  }
+);
+
+const StackNavigator = createSwitchNavigator(
+  {
+    Drawer: DrawerNavigator,
+    Home: HomeScreen,
+    Start: StartScreen,
+  }
+);
+
+const AppNavigator = createAppContainer(StackNavigator);
+
+export default {
+  components: { AppNavigator },
+}
+</script>
---
+++ Screens/Home/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="container">
+    <text class="test"> This is a nice test </text>
+      <button
+        :on-press="onButtonPress"
+        title="Learn More"
+        color="#841584"
+        accessibility-label="Learn more about this purple button"
+      />
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    navigation: {
+      type: Object,
+    }
+  },
+  methods: {
+    onButtonPress() {
+      this.navigation.navigate("Start");
+    }
+  }
+}
+</script>
+
+<style>
+.test {
+  font-size: 30;
+  margin-top: 100;
+}
+</style>
