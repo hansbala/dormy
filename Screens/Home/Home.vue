e4f12ab88d4ac5fa0b30edbd204bd288a8858721
Hans Bala <hansbala@Hanss-MacBook-Pro.local>
2020-04-22 16:01:24 +0530

    some test pictures and home screen basic layout

--- Screens/Home/Home.vue
+++ Screens/Home/Home.vue
@@ -1,12 +1,81 @@
 <template>
     <view class="container">
-        <text class="test">This is a test for Vim</text>
-        <button
-            :on-press="onButtonPress"
-            title="Learn More"
-            color="#841584"
-            accessibility-label="Learn more about this purple button"
-        />
+        <!-- Header that stays fixed at the top -->
+        <view class="header-wrapper">
+            <touchable-opacity class="menu-icon-wrapper">
+                <image
+                    class="menu-icon"
+                    :source="require('../../assets/hamburger_menu.png')"
+                />
+            </touchable-opacity>
+            <text-input class="search-bar" v-model="searchQuery" />
+        </view>
+
+        <!-- Scrollable content (renting, roomates, sub-leasing) -->
+        <scroll-view class="scrollable-content">
+            <!-- Renting Section -->
+            <view class="section-wrapper">
+                <!-- Header Information -->
+                <view class="section-header">
+                    <view class="section-descriptor">
+                        <text class="fnt-grey bold"> Renting near you </text>
+                        <text class="fnt-grey">
+                            Latest based on your location
+                        </text>
+                    </view>
+                    <touchable-opacity class="arrow-icon-wrapper">
+                        <image
+                            class="arrow-icon"
+                            :source="require('../../assets/right_arrow.png')"
+                        />
+                    </touchable-opacity>
+                </view>
+
+                <!-- Horizontall scroll view section -->
+                <scroll-view
+                    :horizontal="true"
+                    class="scroll-section"
+                    id="renting-section"
+                >
+                    <image
+                        class="img-preview"
+                        :source="
+                            require('../../assets/test_pictures/bedroom.jpg')
+                        "
+                    />
+                </scroll-view>
+            </view>
+            <!-- Roomate section -->
+            <view class="section-wrapper">
+                <!-- Header Information -->
+                <view class="section-header">
+                    <view class="section-descriptor">
+                        <text class="fnt-grey bold">Looking for Roommates</text>
+                        <text class="fnt-grey">Most compatible matches </text>
+                    </view>
+                    <touchable-opacity class="arrow-icon-wrapper">
+                        <image
+                            class="arrow-icon"
+                            :source="require('../../assets/right_arrow.png')"
+                        />
+                    </touchable-opacity>
+                </view>
+
+                <!-- Horizontall scroll view section -->
+                <scroll-view
+                    :horizontal="true"
+                    class="scroll-section"
+                    id="renting-section"
+                >
+                    <image
+                        class="img-preview"
+                        :source="
+                            require('../../assets/test_pictures/bedroom.jpg')
+                        "
+                    />
+                </scroll-view>
+            </view>
+        </scroll-view>
     </view>
 </template>
 
@@ -17,6 +86,12 @@ export default {
             type: Object
         }
     },
+    data: function() {
+        return {
+            // Also serves as the placeholder
+            searchQuery: "Search housing, rides, roomates"
+        };
+    },
     methods: {
         onButtonPress() {
             this.navigation.navigate("Start");
@@ -26,8 +101,47 @@ export default {
 </script>
 
 <style>
-.test {
-    font-size: 30;
-    margin-top: 100;
+.header-wrapper {
+    flex-direction: row;
+    margin-top: 50;
+    margin-left: 20;
+    margin-right: 20;
+}
+.menu-icon {
+    width: 40;
+    height: 40;
+}
+.search-bar {
+    flex: 1;
+    border-color: black;
+    border-width: 1;
+    font-size: 16;
+    border-radius: 5;
+    margin-left: 10;
+    padding: 4;
+    color: #a9a9a9;
+}
+.section-header {
+    flex-direction: row;
+    padding: 20;
+    padding-bottom: 0;
+}
+.fnt-grey {
+    color: #2a2a2a;
+}
+.bold {
+    font-weight: bold;
+}
+.arrow-icon-wrapper {
+    flex: 1;
+    justify-content: center;
+    align-items: flex-end;
+}
+.img-preview {
+    width: 150;
+    height: 150;
+    border-radius: 15;
+    margin: 10;
+    margin-right: 0;
 }
 </style>
