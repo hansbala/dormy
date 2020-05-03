e87765e35577bc62350bac3abaca1ac3ad09eb6e
Hans Bala <hansbala@Hanss-MacBook-Pro.local>
2020-05-03 20:10:48 +0530

    Revert "some navigation stuff"
    This reverts commit 79d16ea40ab4fa487fbc7e0ab48a38762454a4f5.

--- Screens/Home/Home.vue
+++ Screens/Home/Home.vue
@@ -148,6 +148,15 @@
                 </scroll-view>
             </view>
         </scroll-view>
+
+        <!-- Bottom navigation section -->
+        <bottom-nav-bar 
+            :searchActive=true
+            :housingActive=false
+            :roommateActive=false
+            :rideShareActive=false
+            :notifActive=false
+            :navigation="this.props.navigation"></bottom-nav-bar>
     </view>
 </template>
 
